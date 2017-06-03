af0f95b155f4819afdb19decd6956c65258c57d7
bvarga92 <bvarga92@gmail.com>
2017-06-03 22:41:24 +0200

    update

--- avr_ucos2/ucos_src/os_cpu_c.d
+++
@@ -1,2 +0,0 @@
-os_cpu_c.o os_cpu_c.d : ucos_src/os_cpu_c.c ucos_src/includes.h ucos_src/os_cpu.h \
-  ucos_src/os_cfg.h ucos_src/ucos_ii.h ucos_src/avr_isr.h
--- avr_ucos2/ucos_src/ucos_ii.d
+++
@@ -1,5 +0,0 @@
-ucos_ii.o ucos_ii.d : ucos_src/ucos_ii.c ucos_src/includes.h ucos_src/os_cpu.h \
-  ucos_src/os_cfg.h ucos_src/ucos_ii.h ucos_src/avr_isr.h \
-  ucos_src/os_core.c ucos_src/os_flag.c ucos_src/os_mbox.c \
-  ucos_src/os_mem.c ucos_src/os_mutex.c ucos_src/os_q.c ucos_src/os_sem.c \
-  ucos_src/os_task.c ucos_src/os_time.c
