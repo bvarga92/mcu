c4778bd96f55c7403f2a5d7e30cb65410ca1137a
bvarga92 <bvarga92@gmail.com>
2017-06-18 11:33:24 +0200

    update

--- stm32f4_audio/main.c
+++ stm32f4_audio/main.c
@@ -7,23 +7,27 @@
 
 int main(void){
 	int16_t outBuffer[2];
-	uint16_t inBuffer[2];
+	uint16_t inBuffer[1];
 	float phase=0;
-	int32_t inSampleCntr=0, outSampleCntr=0;
+	uint8_t ones;
 	HAL_Init();
 	configSystemClock();
 	ledInit();
 	buttonInit();
 	audioOutInit(FS,(uint16_t*)outBuffer,2);
-	audioInInit(FS,(uint16_t*)inBuffer,2);
+	audioInInit(FS,(uint16_t*)inBuffer,1);
 	while(1){
 		if(inTransferComplete){
 			inTransferComplete=0;
-			if(inBuffer[0]>0xF000)
-				ledOn(LED_RED|LED_GREEN);
-			else
-				ledOff(LED_RED|LED_GREEN);
-			inSampleCntr++;
+			ones=0;
+			while(inBuffer[0]){
+				if(inBuffer[0]&0x0001) ones++;
+				inBuffer[0]>>=1;
+			}
+			if(ones> 9) ledOn(LED_BLUE);   else ledOff(LED_BLUE);
+			if(ones>10) ledOn(LED_GREEN);  else ledOff(LED_GREEN);
+			if(ones>11) ledOn(LED_ORANGE); else ledOff(LED_ORANGE);
+			if(ones>12) ledOn(LED_RED);    else ledOff(LED_RED);
 		}
 		if(outTransferHalfComplete){
 			outTransferHalfComplete=0;
@@ -34,15 +38,6 @@ int main(void){
 			outBuffer[1]=cosf(6.28319f*phase)*32767;
 			phase+=440.0f/((float)FS);
 			if(phase>1) phase-=1;
-			outSampleCntr++;
-		}
-		if(inSampleCntr==FS){
-			inSampleCntr=0;
-			ledToggle(LED_BLUE);
-		}
-		if(outSampleCntr==FS){
-			outSampleCntr=0;
-			ledToggle(LED_ORANGE);
 		}
 	}
 	return 1;
