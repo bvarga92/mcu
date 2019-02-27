4a052dd7182b1efb6f63883b208d8b772c2d9975
bvarga92 <bvarga92@gmail.com>
2019-02-27 18:20:22 +0100

    update

---
+++ stm32f103_lsm6dsox/windows_gui/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace lsm6dsox_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.btnRescan = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.pictureBoxAcc = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGyr = new System.Windows.Forms.PictureBox();
+            this.labelTemp = new System.Windows.Forms.Label();
+            this.labelAcc = new System.Windows.Forms.Label();
+            this.labelGyr = new System.Windows.Forms.Label();
+            this.chbLog = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAcc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGyr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(12, 8);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(90, 21);
+            this.cbPorts.TabIndex = 0;
+            // 
+            // btnRescan
+            // 
+            this.btnRescan.Location = new System.Drawing.Point(117, 8);
+            this.btnRescan.Name = "btnRescan";
+            this.btnRescan.Size = new System.Drawing.Size(90, 23);
+            this.btnRescan.TabIndex = 1;
+            this.btnRescan.Text = "Rescan";
+            this.btnRescan.UseVisualStyleBackColor = true;
+            this.btnRescan.Click += new System.EventHandler(this.btnRescan_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(222, 8);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(90, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // pictureBoxAcc
+            // 
+            this.pictureBoxAcc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxAcc.Location = new System.Drawing.Point(12, 88);
+            this.pictureBoxAcc.Name = "pictureBoxAcc";
+            this.pictureBoxAcc.Size = new System.Drawing.Size(300, 130);
+            this.pictureBoxAcc.TabIndex = 3;
+            this.pictureBoxAcc.TabStop = false;
+            // 
+            // pictureBoxGyr
+            // 
+            this.pictureBoxGyr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxGyr.Location = new System.Drawing.Point(12, 243);
+            this.pictureBoxGyr.Name = "pictureBoxGyr";
+            this.pictureBoxGyr.Size = new System.Drawing.Size(300, 130);
+            this.pictureBoxGyr.TabIndex = 5;
+            this.pictureBoxGyr.TabStop = false;
+            // 
+            // labelTemp
+            // 
+            this.labelTemp.AutoSize = true;
+            this.labelTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTemp.Location = new System.Drawing.Point(13, 40);
+            this.labelTemp.Name = "labelTemp";
+            this.labelTemp.Size = new System.Drawing.Size(106, 17);
+            this.labelTemp.TabIndex = 6;
+            this.labelTemp.Text = "Temperature:";
+            // 
+            // labelAcc
+            // 
+            this.labelAcc.AutoSize = true;
+            this.labelAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelAcc.Location = new System.Drawing.Point(78, 68);
+            this.labelAcc.Name = "labelAcc";
+            this.labelAcc.Size = new System.Drawing.Size(166, 17);
+            this.labelAcc.TabIndex = 7;
+            this.labelAcc.Text = "Accelerometer Output";
+            // 
+            // labelGyr
+            // 
+            this.labelGyr.AutoSize = true;
+            this.labelGyr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelGyr.Location = new System.Drawing.Point(91, 223);
+            this.labelGyr.Name = "labelGyr";
+            this.labelGyr.Size = new System.Drawing.Size(140, 17);
+            this.labelGyr.TabIndex = 8;
+            this.labelGyr.Text = "Gyroscope Output";
+            // 
+            // chbLog
+            // 
+            this.chbLog.AutoSize = true;
+            this.chbLog.Checked = true;
+            this.chbLog.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbLog.Location = new System.Drawing.Point(242, 41);
+            this.chbLog.Name = "chbLog";
+            this.chbLog.Size = new System.Drawing.Size(70, 17);
+            this.chbLog.TabIndex = 9;
+            this.chbLog.Text = "Log Data";
+            this.chbLog.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 383);
+            this.Controls.Add(this.chbLog);
+            this.Controls.Add(this.labelGyr);
+            this.Controls.Add(this.labelAcc);
+            this.Controls.Add(this.labelTemp);
+            this.Controls.Add(this.pictureBoxGyr);
+            this.Controls.Add(this.pictureBoxAcc);
+            this.Controls.Add(this.btnRescan);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.btnConnect);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LSM6DSOX Logger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAcc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGyr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.Button btnRescan;
+        private System.Windows.Forms.PictureBox pictureBoxAcc;
+        private System.Windows.Forms.PictureBox pictureBoxGyr;
+        private System.Windows.Forms.Label labelTemp;
+        private System.Windows.Forms.Label labelAcc;
+        private System.Windows.Forms.Label labelGyr;
+        private System.Windows.Forms.CheckBox chbLog;
+    }
+}
+
---
+++ stm32f103_lsm6dsox/windows_gui/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace lsm6dsox_gui
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort comPort = new SerialPort();
+        private double temperature;
+        private double[,] buf = new double[6, 100];
+        private int bufEnd = -1;
+        private int bufValidCnt = 0;
+        private Bitmap bmAcc, bmGyr;
+        private Graphics gAcc, gGyr;
+        private System.IO.StreamWriter file;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void findPorts()
+        {
+            string[] list = SerialPort.GetPortNames();
+            cbPorts.Items.Clear();
+            if (list.Length == 0) return;
+            for (int i = 0; i <= list.GetUpperBound(0); i++) cbPorts.Items.Add(list[i]);
+            Array.Sort(list);
+            cbPorts.Text = list[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            findPorts();
+            bmAcc = new Bitmap(pictureBoxAcc.ClientRectangle.Width, pictureBoxAcc.ClientRectangle.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            bmGyr = new Bitmap(pictureBoxGyr.ClientRectangle.Width, pictureBoxGyr.ClientRectangle.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            gAcc = Graphics.FromImage(bmAcc);
+            gGyr = Graphics.FromImage(bmGyr);
+        }
+
+        private void btnRescan_Click(object sender, EventArgs e)
+        {
+            findPorts();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text.Equals("Connect"))
+            {
+                if (comPort.IsOpen) comPort.Close();
+                if (cbPorts.Items.Count == 0) return;
+                comPort.PortName = Convert.ToString(cbPorts.Text);
+                comPort.BaudRate = 9600;
+                comPort.DataBits = 8;
+                comPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "One");
+                comPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None");
+                comPort.Parity = (Parity)Enum.Parse(typeof(Parity), "None");
+                comPort.DataReceived += new SerialDataReceivedEventHandler(dataReceivedHandler);
+                try
+                {
+                    comPort.Open();
+                    if (chbLog.Checked)
+                    {
+                        file = new System.IO.StreamWriter("lsm6dsox_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".txt");
+                        file.WriteLine("temp_degC\taccX_g\taccY_g\taccZ_g\tgyrX_degps\tgyrY_degps\tgyrZ_degps");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnConnect.BackColor = Color.SpringGreen;
+                btnConnect.Text = "Disconnect";
+                chbLog.Enabled = false;
+            }
+            else
+            {
+                if (comPort.IsOpen) (new Thread(() => { comPort.Close(); })).Start();
+                btnConnect.BackColor = SystemColors.Control;
+                btnConnect.Text = "Connect";
+                if (chbLog.Checked) file.Close();
+                chbLog.Enabled = true;
+            }
+        }
+
+        private void dataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+                string str = ((SerialPort)sender).ReadTo("$");
+                temperature = 25 + Convert.ToInt16(str.Substring(1, 4), 16) / 256.0;
+                bufEnd = (bufEnd + 1) % buf.GetLength(1);
+                buf[0, bufEnd] = Convert.ToInt16(str.Substring(6, 4), 16) * 16.0 / 32767.0; //accX
+                buf[1, bufEnd] = Convert.ToInt16(str.Substring(11, 4), 16) * 16.0 / 32767.0; //accY
+                buf[2, bufEnd] = Convert.ToInt16(str.Substring(16, 4), 16) * 16.0 / 32767.0; //accZ
+                buf[3, bufEnd] = Convert.ToInt16(str.Substring(21, 4), 16) * 2000.0 / 32767.0; //gyrX
+                buf[4, bufEnd] = Convert.ToInt16(str.Substring(26, 4), 16) * 2000.0 / 32767.0; //gyrY
+                buf[5, bufEnd] = Convert.ToInt16(str.Substring(31, 4), 16) * 2000.0 / 32767.0; //gyrZ
+                if (bufValidCnt < buf.GetLength(1)) bufValidCnt++;
+                if (chbLog.Checked) file.WriteLine("{0, 0:E5}\t{1, 1:E5}\t{2, 2:E5}\t{3, 3:E5}\t{4, 4:E5}\t{5, 5:E5}\t{6, 6:E5}", temperature, buf[0, bufEnd], buf[1, bufEnd], buf[2, bufEnd], buf[3, bufEnd], buf[4, bufEnd], buf[5, bufEnd]);
+                BeginInvoke(new Action(() => { plotData(); }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void plotData()
+        {
+            labelTemp.Text = String.Format("Temperature: {0, 0:f1}\u00B0C", temperature);
+            gAcc.Clear(SystemColors.Control);
+            gGyr.Clear(SystemColors.Control);
+            int x = (buf.GetLength(1) - bufValidCnt) * 3;
+            double[] y = { 0, 0, 0, 0, 0, 0 }, yPrev = { 0, 0, 0, 0, 0, 0 };
+            for (int i = buf.GetLength(1) - bufValidCnt; i < buf.GetLength(1); i++, x += 3)
+            {
+                for (int j = 0; j < 6; j++) y[j] = buf[j, (bufEnd + 1 + i) % buf.GetLength(1)];
+                if (i > buf.GetLength(1) - bufValidCnt)
+                {
+                    gAcc.DrawLine(new Pen(System.Drawing.Color.Red, 2), x - 3, 65 - (int)(yPrev[0] * 22), x, 65 - (int)(y[0] * 22));
+                    gAcc.DrawLine(new Pen(System.Drawing.Color.Green, 2), x - 3, 65 - (int)(yPrev[1] * 22), x, 65 - (int)(y[1] * 22));
+                    gAcc.DrawLine(new Pen(System.Drawing.Color.Blue, 2), x - 3, 65 - (int)(yPrev[2] * 22), x, 65 - (int)(y[2] * 22));
+                    gGyr.DrawLine(new Pen(System.Drawing.Color.Red, 2), x - 3, 65 - (int)(yPrev[3] / 10.0), x, 65 - (int)(y[3] / 10.0));
+                    gGyr.DrawLine(new Pen(System.Drawing.Color.Green, 2), x - 3, 65 - (int)(yPrev[4] / 10.0), x, 65 - (int)(y[4] / 10.0));
+                    gGyr.DrawLine(new Pen(System.Drawing.Color.Blue, 2), x - 3, 65 - (int)(yPrev[5] / 10.0), x, 65 - (int)(y[5] / 10.0));
+                }
+                for (int j = 0; j < 6; j++) yPrev[j] = y[j];
+            }
+            pictureBoxAcc.Image = bmAcc;
+            pictureBoxGyr.Image = bmGyr;
+        }
+
+    }
+}
