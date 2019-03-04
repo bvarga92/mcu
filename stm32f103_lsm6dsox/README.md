17c87f74b9195d7516f98dd8f27ca93971531254
bvarga92 <bvarga92@gmail.com>
2019-03-04 19:57:37 +0100

    update

--- stm32f103_lsm6dsox/README.md
+++ stm32f103_lsm6dsox/README.md
@@ -1,1 +1,3 @@
+![gui_screenshot](https://raw.githubusercontent.com/bvarga92/mcu/main/stm32f103_lsm6dsox/gui_screenshot.png)
+
 ![blokkvazlat](https://raw.githubusercontent.com/bvarga92/mcu/main/stm32f103_lsm6dsox/blokkvazlat.png)
--- stm32f103_lsm6dsox/machine_learning/generate_features.m
+++ stm32f103_lsm6dsox/machine_learning/generate_features.m
@@ -26,9 +26,117 @@ F={
     {'accX_peaksN',0.5}
     {'accX_min'}
     {'accX_max'}
+    {'accY_mean'}
+    {'accY_var'}
+    {'accY_energy'}
+    {'accY_pk2pk'}
+    {'accY_cross',0}
+    {'accY_crossP',0}
+    {'accY_crossN',0}
+    {'accY_peaks',0.5}
+    {'accY_peaksP',0.5}
+    {'accY_peaksN',0.5}
+    {'accY_min'}
+    {'accY_max'}
+    {'accZ_mean'}
+    {'accZ_var'}
+    {'accZ_energy'}
+    {'accZ_pk2pk'}
+    {'accZ_cross',0}
+    {'accZ_crossP',0}
+    {'accZ_crossN',0}
+    {'accZ_peaks',0.5}
+    {'accZ_peaksP',0.5}
+    {'accZ_peaksN',0.5}
+    {'accZ_min'}
+    {'accZ_max'}
+    {'acc_mean'}
+    {'acc_var'}
+    {'acc_energy'}
+    {'acc_pk2pk'}
+    {'acc_cross',0}
+    {'acc_crossP',0}
+    {'acc_crossN',0}
+    {'acc_peaks',0.5}
+    {'acc_peaksP',0.5}
+    {'acc_peaksN',0.5}
+    {'acc_min'}
+    {'acc_max'}
+    {'acc2_mean'}
+    {'acc2_var'}
+    {'acc2_energy'}
+    {'acc2_pk2pk'}
+    {'acc2_cross',0}
+    {'acc2_crossP',0}
+    {'acc2_crossN',0}
+    {'acc2_peaks',0.25}
+    {'acc2_peaksP',0.25}
+    {'acc2_peaksN',0.25}
+    {'acc2_min'}
+    {'acc2_max'}
+    {'gyrX_mean'}
+    {'gyrX_var'}
+    {'gyrX_energy'}
+    {'gyrX_pk2pk'}
+    {'gyrX_cross',0}
+    {'gyrX_crossP',0}
+    {'gyrX_crossN',0}
+    {'gyrX_peaks',100}
+    {'gyrX_peaksP',100}
+    {'gyrX_peaksN',100}
+    {'gyrX_min'}
+    {'gyrX_max'}
+    {'gyrY_mean'}
+    {'gyrY_var'}
+    {'gyrY_energy'}
+    {'gyrY_pk2pk'}
+    {'gyrY_cross',0}
+    {'gyrY_crossP',0}
+    {'gyrY_crossN',0}
+    {'gyrY_peaks',100}
+    {'gyrY_peaksP',100}
+    {'gyrY_peaksN',100}
+    {'gyrY_min'}
+    {'gyrY_max'}
+    {'gyrZ_mean'}
+    {'gyrZ_var'}
+    {'gyrZ_energy'}
+    {'gyrZ_pk2pk'}
+    {'gyrZ_cross',0}
+    {'gyrZ_crossP',0}
+    {'gyrZ_crossN',0}
+    {'gyrZ_peaks',100}
+    {'gyrZ_peaksP',100}
+    {'gyrZ_peaksN',100}
+    {'gyrZ_min'}
+    {'gyrZ_max'}
+    {'gyr_mean'}
+    {'gyr_var'}
+    {'gyr_energy'}
+    {'gyr_pk2pk'}
+    {'gyr_cross',0}
+    {'gyr_crossP',0}
+    {'gyr_crossN',0}
+    {'gyr_peaks',100}
+    {'gyr_peaksP',100}
+    {'gyr_peaksN',100}
+    {'gyr_min'}
+    {'gyr_max'}
+    {'gyr2_mean'}
+    {'gyr2_var'}
+    {'gyr2_energy'}
+    {'gyr2_pk2pk'}
+    {'gyr2_cross',0}
+    {'gyr2_crossP',0}
+    {'gyr2_crossN',0}
+    {'gyr2_peaks',10000}
+    {'gyr2_peaksP',10000}
+    {'gyr2_peaksN',10000}
+    {'gyr2_min'}
+    {'gyr2_max'}
 };
 
-%% beolvasas es feature generalas
+%% beolvasas es feature szamitas
 features=[];
 for ff=1:length(files)
     fp=fopen(files{ff}{1});
@@ -159,4 +267,36 @@ for ff=1:length(files)
     end
     features=[features ; features_ff files{ff}{2}*ones(size(features_ff,1),1)];
 end
-features
+
+%% ARFF fajl generalasa
+fp=fopen('features.arff','wt');
+fprintf(fp,'@relation ''MLC''\n\n');
+for ii=1:length(F)
+    fprintf(fp,'@attribute F%d_%s numeric\n',ii,F{ii}{1});
+end
+fprintf(fp,'@attribute class {');
+classes=[];
+for ii=1:length(files)
+    classes(end+1)=files{ii}{2};
+end
+classes=unique(classes);
+fprintf(fp,'%d',classes(1));
+for ii=2:length(classes)
+    fprintf(fp,',%d',classes(ii));
+end
+fprintf(fp,'}\n\n');
+fprintf(fp,'@data\n');
+for ii=1:size(features,1)
+    fprintf(fp,'%f, ',features(ii,1:end-1));
+    fprintf(fp,'%d\n',features(ii,end));
+end
+fclose(fp);
+
+%% dontesi fa
+for ii=1:length(F)
+    PredictorNames{ii}=F{ii}{1};
+end
+treeClassifier=ClassificationTree.fit(features(:,1:end-1),features(:,end),'PredictorNames',PredictorNames);
+output=treeClassifier.predict(features(:,1:end-1));
+treeClassifier.view
+fprintf('Misclassifications: %d\n\n',sum(output~=features(:,end)));
